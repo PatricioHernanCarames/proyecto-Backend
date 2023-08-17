c6213694da3c4174f2efda035d8979661e7bd12f
Patricio Carames <patriciocarames@gmail.com>
2023-08-17 18:59:56 -0300

    mejoras en app.js

--- src/app.js
+++ src/app.js
@@ -1,23 +1,22 @@
 import express from "express";
-import productsRouter from "../routes/products.router.js"
-import cartRouter from "../routes/cart.router.js"
-import __dirname from "./utils.js"
-import viewsRouter from "../routes/view.router.js"
+import productsRouter from "../routes/products.router.js";
+import cartRouter from "../routes/cart.router.js";
+import viewsRouter from "../routes/view.router.js";
 
 const app = express();
 const server = 8080;
 
-
-
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
 
-app.engine("handlebars", handlebars.engine());
+
 app.set("view engine", "handlebars");
-app.set("views",__dirname + "/views")
+app.set("views", `${__dirname}/views`); 
+
 
 app.use("/", viewsRouter);
 
