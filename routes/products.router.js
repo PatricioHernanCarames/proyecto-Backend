7f86acaef441be7c40ee5a04d9daf553671ec593
Patricio Carames <patriciocarames@gmail.com>
2023-08-17 19:05:56 -0300

    correccion en el prods router

--- routes/products.router.js
+++ routes/products.router.js
@@ -1,9 +1,8 @@
-import {Router, json} from 'express';
-import ProductManager from '../src/api/Product.Manager';
+import { Router, json } from 'express';
+import ProductManager from '../src/api/Product.Manager.js'; // Check the path to the ProductManager file
 
 
-
-const filePath = '.api/products.json';
+const filePath = `${__dirname}/api/products.json`; 
 const productManager = new ProductManager(filePath);
 
 const productsRouter = Router();
@@ -11,59 +10,41 @@ const productsRouter = Router();
 productsRouter.use(json());
 
 
-productsRouter.get("api/products", (req, res) => {
-    const limit = req.query.limit;
-  
-    try {
-      if (!limit) {
-        const products = productManager.getProducts();
-        res.send(products);
-      } else {
-        const limitNum = parseInt(limit, 10);
-  
-        if (isNaN(limitNum)) {
-          res.status(400).json({ error: "invalid limit number" });
-        } else {
-          const prods = productManager.getProducts().slice(0, limit);
-          return res.send.json(prods);
-        }
-      }
-    } catch (error) {
-      res.status(500).json({ error: "internal server error" });
-    }
-  });
-  
-  productsRouter.get('/api/products/:pid', async (req,res) =>{
-      try{
-      const {pid} = req.params;
-      
-          const item = await productManager.getProductById(pid)
-          res.status(200).send(item)
-      }catch(error){
-  
-          res.status(400).json({error : 'invalid id'})
-  
-      }
-      
-  })
-  
-  productsRouter.post("/", uploader.single("thumbnail"), async (req, res) => {
-    const { title, description, price, stock, code, status, category } = req.body;
-  
-    
-  
-    let thumbnail=(req.file.path);
-    req.body.thumbnail = thumbnail;
-    try {
-      const task = productsModel(req.body);
-      console.log(task);
-  
-      await task.save();
-  
-      res.status(201).send(task);
-    } catch (e) {
-      res.status(500).send({ status: "error", payload: e.message });
-    }
-  });
+productsRouter.get("/api/products", (req, res) => {
+  const limit = req.query.limit;
 
-  export default productsRouter
+  try {
+    if (!limit) {
+      const products = productManager.getProducts();
+      res.send(products);
+    } else {
+      const limitNum = parseInt(limit, 10);
+
+      if (isNaN(limitNum)) {
+        res.status(400).json({ error: "Invalid limit number" });
+      } else {
+        
+        const prods = productManager.getProducts().slice(0, limitNum);
+        res.json(prods); 
+      }
+    }
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
+
+
+productsRouter.get('/api/products/:pid', async (req, res) => {
+  try {
+    const { pid } = req.params;
+
+    const item = await productManager.getProductById(pid);
+    res.status(200).send(item);
+  } catch (error) {
+    res.status(400).json({ error: 'Invalid id' });
+  }
+});
+
+
+
+export default productsRouter;
