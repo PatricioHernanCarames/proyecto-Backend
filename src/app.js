06113db45bfa0fd924acdaf16228d28b04ffaf78
Patricio Carames <patriciocarames@gmail.com>
2023-08-17 19:16:10 -0300

    correcciones

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { join } from "path";
 import productsRouter from "../routes/products.router.js";
 import cartRouter from "../routes/cart.router.js";
 import viewsRouter from "../routes/view.router.js";
+import __dirname from './utils.js';
 
 const app = express();
 const server = 8080;
@@ -10,13 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.set("view engine", "handlebars");
+app.set("views", join(__dirname, 'views'));
+
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartRouter);
-
-
-app.set("view engine", "handlebars");
-app.set("views", `${__dirname}/views`); 
-
 
 app.use("/", viewsRouter);
 
